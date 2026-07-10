c9143f0a91e24c9c0a2581efc43cbfea8e25ac75
Elephant-on-github <165417719+Elephant-on-github@users.noreply.github.com>
2026-07-10 23:42:42 +0100

    :sparkles: some more work ig

--- fly.gd
+++ fly.gd
@@ -5,12 +5,15 @@ signal collided(with_sprite)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print(self.name)
 	pass # Replace with function body.
 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if name == "FlyTemplate":
+		return
 	# In the cloned sprite's script
 	if Input.is_action_just_pressed("ui_select"):
 		var overlapping = $Area2D.get_overlapping_areas() + $Area2D.get_overlapping_bodies()
@@ -20,5 +23,12 @@ func _process(delta):
 				if other.name == "swatterarea":
 					collided.emit(other)
 					queue_free()
-					break
+					return
 #^ (partially) from the AI
+
+
+func _on_label_finished():
+	if name == "FlyTemplate":
+		return
+	queue_free()
+	
--- main.gd
+++ main.gd
@@ -1,9 +1,10 @@
 extends Control
 
+var stop : bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	%prizes.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,15 +13,18 @@ var spawn_delay: float = 0.1
 
 func _process(delta):
 	spawn_timer += delta
-	
-	if spawn_timer >= spawn_delay:
+	if stop:
+		pass
+	elif spawn_timer >= spawn_delay:
 		# Get a random position on the screen
-		var random_pos = Vector2(randf_range(0, get_viewport_rect().size.x), 
-								 randf_range(0, get_viewport_rect().size.y))
+		var random_pos = Vector2(randf_range(350, get_viewport_rect().size.x - 20), 
+								 randf_range(20, get_viewport_rect().size.y - 20))
 
 		# Create a duplicate of the sprite
 		var sprite_clone = $FlyTemplate.duplicate()
 		sprite_clone.visible = true
+		%timer.finished.connect(sprite_clone._on_label_finished)
+		sprite_clone.name = "clone"
 		# Set its position to the random location
 		sprite_clone.position = random_pos
 		
@@ -29,3 +33,9 @@ func _process(delta):
 		
 		# Reset the timer
 		spawn_timer = 0.0
+
+
+func _on_label_finished():
+	%prizes.visible = true
+	stop = true
+	
