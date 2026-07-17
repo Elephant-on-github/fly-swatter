13ae11fc805200f1707b6d73befa4caa8776a524
Elephant-on-github <165417719+Elephant-on-github@users.noreply.github.com>
2026-07-17 13:54:00 +0100

    :rocket: credits and how to

--- helpscreen.gd
+++ helpscreen.gd
@@ -1,11 +1,15 @@
-extends TextureRect
+extends NinePatchRect
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	self.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_button_pressed():
+	self.visible = true
